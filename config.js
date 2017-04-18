9ef48e39b71ca9c6bab58b042d67aca6a00054c6
Rune Tvilum <rune@addin.dk>
2017-04-18 09:38:54 +0200

    conf

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+    access_token: "CAAGarz3jyQ8BACstkLzjwrujAvw3lPjgrSPYrjwe3nQKfVif5iPcFFukucPNc6sMaz6hSxuaK5urkeApEwYAdEjMvaOuPS5kt4ANYXSakJ2TJZBe2a4uAVlPuLhFvutEHDMN6OwS2y6oYlWSwrznOUiwAgIDXm2ZAXw37AhCASSlogvBQ6",
+    firebase_token: 'f8Oh6eKLjAmZquQaZxrU69Kj3UdMCGeGzFH8NtCN'
+};
+
+module.exports = config;
